c9cd20ffc3ff589e8598d1ca542e0b0c1e29656b
ushuwa <jobojoshuajireh13@gmail.com>
2026-04-06 19:33:04 +0800

    Update DATABASE_URL to pull from environment variable

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "devkey123")
 
-    DATABASE_URL = os.getenv("postgresql://analytics_h94f_user:jFVlxpFqygGU6BwOBbu8vTv9VCGhwE7w@dpg-d79odlmuk2gs73ehliu0-a/analytics_h94f")
+    # Pull from environment variable
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
-    # Fix for SQLAlchemy (Render uses postgres:// sometimes)
+    # Fix for SQLAlchemy / psycopg2 if URL starts with postgres://
     if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
