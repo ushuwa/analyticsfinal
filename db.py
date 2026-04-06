7d59a5c0a63ae2e5e3b76cc9fb6db5b440890055
ushuwa <jobojoshuajireh13@gmail.com>
2026-04-06 18:35:51 +0800

    Simplify database connection using DATABASE_URL

--- db.py
+++ db.py
@@ -2,11 +2,5 @@ import psycopg2
 from config import Config
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=Config.DB_HOST,
-        port=Config.DB_PORT,
-        database=Config.DB_NAME,
-        user=Config.DB_USER,
-        password=Config.DB_PASS
-    )
+    conn = psycopg2.connect(Config.DATABASE_URL)
     return conn
